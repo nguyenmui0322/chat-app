d7573d1a747d6dc6d22acae86b5d1a39b5e3bc07
mui <nvanmui8@gmail.com>
2024-03-27 14:05:34 +0700

    Đăng nhập

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,8 +1,12 @@
+const userMiddleware = require("../middlewares/user.middleware");
+
 const homeRoutes = require("./home.route");
 const chatRoutes = require("./chat.route");
 const userRoutes = require("./user.route");
 
 module.exports = (app) => {
+  app.use(userMiddleware.infoUser);
+
   app.use("/", homeRoutes);
 
   app.use("/chat", chatRoutes);
