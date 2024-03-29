22874b324f64b20aafadf98c74cdaf59128cc97c
mui <nvanmui8@gmail.com>
2024-03-29 22:46:13 +0700

    Typing

--- public/js/chat.js
+++ public/js/chat.js
@@ -79,7 +79,21 @@ if (emojiPicker) {
     const icon = event.detail.unicode;
     inputChat.value = inputChat.value + icon;
   });
+
+  // Input Keyup
+  inputChat.addEventListener("keyup", () => {
+    socket.emit("CLIENT_SEND_TYPING", "show");
+  });
+  // End Input Keyup
 }
 // End Insert Icon To Input
 
 // End Show Icon Chat
+
+// SERVER_RETURN_TYPING
+
+socket.on("SERVER_RETURN_TYPING", (data) => {
+  console.log(data);
+});
+
+// End SERVER_RETURN_TYPING
