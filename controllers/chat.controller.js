ce808498577a13f3d2a9780d9345effa3db5973f
mui <nvanmui8@gmail.com>
2024-03-31 22:21:22 +0700

    Tách socket sang folder

--- controllers/chat.controller.js
+++ controllers/chat.controller.js
@@ -1,54 +1,18 @@
 const Chat = require("../models/chat.model");
 const User = require("../models/user.model");
 
-const uploadToCloudinary = require("../helpers/uploadToCloudinary");
+const chatSocket = require("../sockets/chat.socket");
 
 // [GET] /chat
 module.exports.index = async (req, res) => {
-  const userId = res.locals.user.id;
-  const fullName = res.locals.user.fullName;
-
   // SocketIO
-  _io.once("connection", (socket) => {
-    socket.on("CLIENT_SEND_MESSAGE", async (data) => {
-      let images = [];
-
-      for (const imageBuffer of data.images) {
-        const link = await uploadToCloudinary(imageBuffer);
-        images.push(link);
-      }
-
-      const chat = new Chat({
-        user_id: userId,
-        content: data.content,
-        images: images,
-      });
-      await chat.save();
-
-      _io.emit("SERVER_RETURN_MESSAGE", {
-        userId: userId,
-        fullName: fullName,
-        content: data.content,
-        images: images,
-      });
-    });
-
-    // Typing
-    socket.on("CLIENT_SEND_TYPING", async (type) => {
-      socket.broadcast.emit("SERVER_RETURN_TYPING", {
-        userId: userId,
-        fullName: fullName,
-        type: type,
-      });
-    });
-    // End Typing
-  });
+  chatSocket(res);
   // End SocketIO
 
+  // Lấy data từ database
   const chats = await Chat.find({
     deleted: false,
   });
-
   for (const chat of chats) {
     const infoUser = await User.findOne({
       _id: chat.user_id,
